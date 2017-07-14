38862884fe01e779eb9a52d0a19539f05a726832
iLan Epstein <iLan@mentalfabrications.com>
2017-07-14 10:11:12 -0700

    Adding shell script to create symlinks

---
+++ init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ln -s ~/.dotfiles/.bash_profile ~/.bash_profile
+ln -s ~/.dotfiles/.git-completion.sh ~/.git-completion.sh
+ln -s ~/.dotfiles/.git-prompt.sh ~/.git-prompt.sh
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/.gitignore_global ~/.gitignore_global
+
+source ~/.bash_profile
